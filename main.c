f9281bc03a3d7810e81e6f04bbda779c1b637a43
richard <lenovo@lenovo.(none)>
2012-02-27 17:11:28 +0000

    ionly events

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
 int
 main(int argc, char *argv[])
 {
   struct dirent *node;
   DIR *input = opendir("/dev/input");
+  int count = 0;
+  const char *event_string = "event";
+  const int event_strlen = strlen(event_string);
 
+  int temp;
   while((node = readdir(input)) != NULL)
   {
-    printf("x");
+    if(strncmp(node->d_name, event_string, event_strlen) != 0)
+      continue;
+    printf("%s\n", node->d_name);
+    count++;
   }
 
   closedir(input);
