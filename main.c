9592143fe9c1bfc34d0ec24099be1958513814fd
richard <lenovo@lenovo.(none)>
2012-02-27 16:21:20 +0000

    list nodes

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <dirent.h>
 
 int
 main(int argc, char *argv[])
 {
+  struct dirent *node;
+  DIR *input = opendir("/dev/input");
+
+  while((node = readdir(input)) != NULL)
+  {
+    printf("x");
+  }
+
+  closedir(input);
   printf("h");
   return 0;
 }
